4e898fcfe0beae26f02ece7b7f928cb9320c14f4
Lea Vaiana <leavaiana@gmail.com>
2019-01-02 16:27:54 +0100

    onclick events, button, displayDate

---
+++ Modify_DOM.js
@@ -0,0 +1,11 @@
+function firstFunction(){
+    document.getElementById('output').innerHTML = 'Clicked First Button';
+  }
+
+function secondFunction(){
+    document.getElementById('output').innerHTML = 'Clicked Second Button';
+  }
+
+function displayDate(){
+    document.getElementById("demo").innerHTML=Date();
+}
